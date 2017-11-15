53be7bacfde76dc5ffad9ac811d1761c3ba4af98
hakoares <s326169@stud.hioa.no>
2017-11-15 15:17:49 +0100

    Merge branch 'master' of https://github.com/Nudua/friminutt

--- js/site.js
+++ js/site.js
@@ -1,1 +1,10 @@
-//Blank for now
+// /* FAQ - SHOW/HIDE FUNCTION */
+// function displayFaqAnswer(sender) {
+//     let svar = document.getElementById(sender);
+//
+//     if(svar.style.display != 'block') {
+//         svar.style.display = 'block';
+//     } else {
+//         svar.style.display = 'none';
+//     }
+// }
