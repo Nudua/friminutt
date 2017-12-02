94cbea491883555715a8b5d652a1a08e8c5a9f6d
Tor Ramstad <tor.audun@gmail.com>
2017-12-03 00:09:18 +0100

    notify me

--- merged/js/site.js
+++ merged/js/site.js
@@ -1,54 +1,16 @@
-// /* FAQ - SHOW/HIDE FUNCTION */
-// function displayFaqAnswer(sender) {
-//     let svar = document.getElementById(sender);
-//
-//     if(svar.style.display != 'block') {
-//         svar.style.display = 'block';
-//     } else {
-//         svar.style.display = 'none';
-//     }
-// }
+function init() {
 
-function toggleNewMobileMenu() {
-    let dropdown = document.getElementById("mobileMenuDropdown");
-    let dropdownImage = document.getElementById("mobileMenuButtonImage");
-    let menuButton = document.getElementById("mobileMenuText");
+    let form = document.getElementById("subscribeForm");
 
-    if (dropdown.style.display == "none" || dropdown.style.display == "") {
-        dropdown.style.display = "inherit";
+    form.onsubmit = function(event) {
+        event.preventDefault();
 
-        dropdownImage.src = "img/hamburger-menu-close.png";
-        menuButton.innerHTML = "Close";
+
+        let input = document.getElementById("subscribeInput");
+        input.value = "";
+
+        alert("Thank you! You will be notified when the app releases.");
+
+        return false;
     }
-    else {
-        dropdown.style.display = "none";
-        dropdownImage.src = "img/hamburger-menu-open.png";
-        menuButton.innerHTML = "Menu";
-    }
-}
-
-//Fadder Javascript
-function Fadder(name, info, imagePath) {
-    this.name = name;
-    this.info = info;
-    this.imagePath = imagePath;
-}
-
-let faddere = {};
-faddere[0] = new Fadder("Anders", "Han går dataingeniør på HiOA. Han elsker alt som finnes av ballsport, fotball, innebandy, basketball you name it! Ansvarlig for planlegging av turneringer i helgene der alle fadderbarn kan delta og vinne premier.", "img/anders.jpg");
-faddere[1] = new Fadder("Si-Jane Woo", "Hun går dataingeniør på HiOA. Hun er tidliger profesjonell dans utøver og hun elsker å lære bort det hun kan. Hun har også en lidenskap for mat og snakker fire språk flytende.","img/cj.jpg");
-faddere[2] = new Fadder("Kristoffer Farstad", "Han går dataingeniør på HiOA. Han liker dataspill, fotball og fjellturer. Han er aktiv i ledelsen og planlegger stadig større events for Friminutt, slik at alle skal få en fantastisk opplevelse.", "img/kristoffer.jpg");
-
-function changeFadder(index) {
-    let fadder = faddere[index];
-
-    let name = document.getElementById("fadder-name");
-    let text = document.getElementById("fadder-text");
-    let image = document.getElementById("fadder-image");
-
-    name.innerHTML = fadder.name;
-    text.innerHTML = fadder.info;
-    image.src = fadder.imagePath;
-
-    //console.log(fadder);
 }
